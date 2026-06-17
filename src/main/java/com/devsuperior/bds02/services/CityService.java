20af018709342307827ae7975326614732d607fa
pedrotrevisan <52826302+pedrotrevisan@users.noreply.github.com>
2026-06-17 11:48:10 -0300

    Implementa controllers, services, repositories e exception handler

---
+++ src/main/java/com/devsuperior/bds02/services/CityService.java
@@ -0,0 +1,49 @@
+package com.devsuperior.bds02.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.bds02.dto.CityDTO;
+import com.devsuperior.bds02.entities.City;
+import com.devsuperior.bds02.repositories.CityRepository;
+import com.devsuperior.bds02.services.exceptions.DatabaseException;
+import com.devsuperior.bds02.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class CityService {
+
+    @Autowired
+    private CityRepository repository;
+
+    @Transactional(readOnly = true)
+    public List<CityDTO> findAll() {
+        return repository.findAllByOrderByNameAsc()
+                .stream()
+                .map(CityDTO::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public CityDTO insert(CityDTO dto) {
+        City entity = new City();
+        entity.setName(dto.getName());
+        entity = repository.save(entity);
+        return new CityDTO(entity);
+    }
+
+    public void delete(Long id) {
+        if (!repository.existsById(id)) {
+            throw new ResourceNotFoundException("Cidade não encontrada. Id: " + id);
+        }
+        try {
+            repository.deleteById(id);
+        } catch (DataIntegrityViolationException e) {
+            throw new DatabaseException("Integrity violation");
+        }
+    }
+}
---
+++ src/main/java/com/devsuperior/bds02/services/exceptions/ResourceNotFoundException.java
@@ -0,0 +1,5 @@
+package com.devsuperior.bds02.services.exceptions;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) { super(message); }
+}
